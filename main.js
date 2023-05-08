526b8d31b672cb6cca9c7db60c297c26f7b62072
Felipe Martins Bonifacio <felipemartins.py@gmail.com>
2023-05-08 00:36:48 -0300

    Alterado as tags p das mensagem de erro e sucesso, além da lógica do javascript

--- main.js
+++ main.js
@@ -11,36 +11,22 @@ form.addEventListener('submit', function(e){
 
     e.preventDefault();
 
-    const msgSucesso = "Formulário válido, o valor de A("+ NumA.value +") é menor do que B("+ NumB.value +")";
+    const msg = document.querySelector('.msg');
+    msg.className='msg';
+
+const msgSucesso = "Formulário válido, o valor de A("+ NumA.value +") é menor do que B("+ NumB.value +")";
     const msgError = "O formulário é inválido, pos o Número A("+ NumA.value +") é maior ou igual que o número B("+ NumB.value +")";
 
     valido = validaNumero(NumA.value, NumB.value);
     if(valido){
-        const PmsgSucesso = document.querySelector('.success-msg');
-        PmsgSucesso.innerHTML = msgSucesso;
-        PmsgSucesso.style.display = 'block'
-
-        NumA.value = '';
-        NumB.value = '';
+        msg.innerHTML = msgSucesso;
+        msg.classList.add('success');
 
     }else{
-
-        document.querySelector('.error-msg').innerHTML = msgError;
-        document.querySelector('.success-msg').style.display = 'none'
-        document.querySelector('.error-msg').style.display = 'block';
+        document.querySelector('.msg').innerHTML = msgError;
+        document.querySelector('.msg').classList.add('error');
     }
-})
 
-NumB.addEventListener('keyup', function(e){
-    console.log(e.target.value);
-
-    valido = validaNumero(NumA.value, e.target.value);
-        if(!valido){
-        NumB.classList.add('error');
-        document.querySelector('.error-msg').style.display = 'block';
-        document.querySelector('.error-msg').innerHTML = "O formulário é inválido, pos o Número A("+ NumA.value +") é maior ou igual que o número B("+ NumB.value +")";
-    } else{
-        NumB.classList.remove('error');
-        document.querySelector('.error-msg').style.display = 'none';
-    }
+    NumA.value = '';
+    NumB.value = '';
 })
