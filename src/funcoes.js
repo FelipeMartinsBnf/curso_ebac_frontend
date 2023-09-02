8c0f28c0788aa2df96a7330c4074e05d4585d1fe
Felipe Martins Bonifacio <felipemartins.py@gmail.com>
2023-09-02 15:01:17 -0300

    Commit Inicial

---
+++ src/funcoes.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplicar = (x, y) => x * y;
+function saldacao(nome) {
+    return `Olá ${nome}!`;
+}
+console.log(multiplicar(100, 5));
+console.log(saldacao("Larissa"));
---
+++ src/funcoes.ts
@@ -0,0 +1,9 @@
+const multiplicar = (x:number, y:number):number => x * y;
+
+function saldacao(nome:string):string{
+    return `Olá ${nome}!`;
+}
+
+
+console.log(multiplicar(100, 5));
+console.log(saldacao("Larissa"));
